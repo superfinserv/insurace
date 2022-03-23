a5eeb47315d0604e3c688e18a8cb7d8f8d38e4f9
Praveen Patidar <superfinserv@host.superfinserv.in>
2022-03-23 13:13:15 +0530

    Add seatings

---
+++ config/cred.php
@@ -0,0 +1,8 @@
+<?php
+ return array (
+  'signzy' => array(
+           'baseurl' => 'https://signzy.tech/',
+           'username'=> 'Superfinserv_Test',
+           'password'=>'P2LoVjhgOcOECVVjlbh'
+      ),
+) ;
