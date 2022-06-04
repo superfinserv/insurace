bb958adcd01d0fe4f2c4815e226ec5c675d1c083
Praveen Patidar <superfinserv@host.superfinserv.in>
2022-06-04 09:04:43 +0530

    FGI TW Live & git ignore update

---
+++ resources/views/termInsurance/index.blade.php
@@ -0,0 +1,29 @@
+ @extends($layout)
+    @section('content')
+            <main role="main">
+				<section class="term-baner-animation" style="max-height: 100%;margin-bottom: 125px;">
+					<div class="containe">
+						<div class="row ptb">
+							
+							<div class="col-md-12 col-sm-12 page-heading-h2" style="height:2050px;">
+							  <h2 class="text-center">Find the <span style="color: #C52118;font-weight: 600;">"best match"</span> Term insurance for you.</h2>
+							   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdlHz_WYC2ucihHZ8-YKQHhD1CewJmN3YAC0I4_Oq0J8TTRZQ/viewform?embedded=true" 
+							          width="100%" height="100%" style="border:none" >Loading…</iframe>
+							</div>
+							<div class="col-md-3 col-sm-3">&nbsp;
+							</div>
+						</div>
+					</div>
+				</section>
+				<!--<section class="view-btn ptb">-->
+				<!--	<div class="container">-->
+				<!--		<div class="row text-center">-->
+				<!--			<a href="{{url('health-insurance-detail')}}" class="view-btn">VIEW DETAIL</a>-->
+				<!--		</div>-->
+				<!--	</div>-->
+				<!--</section>-->
+				
+			</main>
+	
+				
+@endsection
